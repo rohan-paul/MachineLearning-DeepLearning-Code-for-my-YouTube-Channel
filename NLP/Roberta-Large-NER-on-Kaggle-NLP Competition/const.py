edea7ef7bd6b6d745bcb820ef0806782337348b6
Rohan Paul <rohan-paul@users.noreply.github.com>
2023-03-29 02:36:00 +0530

    "updating"

--- NLP/Roberta-Large-NER-on-Kaggle-NLP Competition/const.py
+++ NLP/Roberta-Large-NER-on-Kaggle-NLP Competition/const.py
@@ -1,34 +1,25 @@
 import os
 import torch
 
+# You can run the Notebook `Pytorch-Roberta_Large.ipynb`  either Locally or in Kaggle - Just modify the 'ROOT_DIR' variable to properly refer to the dataset
 # ROOT_DIR = '../input/feedback-prize-2021/' # Kaggle
 ROOT_DIR = '../input/' # local
 
-# This is used to download the model from the huggingface hub
 # MODEL_NAME = 'roberta-large'
 MODEL_NAME = 'roberta-base'
 
-# Path where to download the model
 MODEL_PATH = 'model'
 
-RUN_NAME = f"{MODEL_NAME}-4"
+RUN_NAME = f"{MODEL_NAME}"
 
-# Max length for the tokenization and the model
-# For BERT-like models it's 512 in general
 MAX_LEN = 512
 
-# The authorized overlap between two part of the context when splitting it is needed.
-# The overlapping tokens when chunking the texts
-# Possibly a power of 2 would have been better
 DOC_STRIDE = 128
 
-# Training configuration
-# 5 epochs with different learning rates (inherited from Chris')
-# Haven't tried variations yet
-config = {'train_batch_size': 1,
+config = {'train_batch_size': 4,
           'valid_batch_size': 1,
-          'epochs': 1,
-          'learning_rates': [1],
+          'epochs': 5,
+          'learning_rates': [2.5e-5, 2.5e-5, 2.5e-6, 2.5e-6, 2.5e-7],
           'max_grad_norm': 10,
           'device': 'cuda' if torch.cuda.is_available() else 'cpu',
           'model_name': MODEL_NAME,
