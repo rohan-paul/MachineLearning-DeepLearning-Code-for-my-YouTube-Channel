01a2a3ffb0f843596b29771fed587a3d5e55ad40
Rohan Paul <rohan-paul@users.noreply.github.com>
2023-05-12 02:09:01 +0530

    "updating"

--- Computer_Vision/DCGAN-from-Scratch-with-PyTorch/utils.py
+++ Computer_Vision/DCGAN-from-Scratch-with-PyTorch/utils.py
@@ -50,24 +50,52 @@ def get_data_loader(batch_size):
 
 
 def plot_images(epoch, path, num_test_samples, generator, device):
+    """
+    This function generates a set of images using the provided generator model, and plots
+    them in a grid structure.
+
+    Parameters
+    ----------
+    epoch : int
+        The current training epoch. Used for labeling the plot.
+    path : str
+        The path where the images will be saved.
+    num_test_samples : int
+        The number of images to generate and plot.
+    generator : torch.nn.Module
+        The generator model to use for creating the images.
+    device : torch.device
+        The device (CPU or GPU) where the generator model is located.
+
+    Returns
+    -------
+    None
+    """
+    # Generate a random noise tensor with shape (num_test_samples, 100, 1, 1)
+    # which will be used as input to the generator model to create images.
     z = torch.randn(num_test_samples, 100, 1, 1, device=device)
 
+    # Calculate the size of the grid to plot based on the number of test samples.
     plot_grid_size = int(math.sqrt(num_test_samples))
 
-    title = None
-
+    # Generate the images using the generator model.
     generated_fake_images = generator(z)
 
+    # Append "variable_noise/" to the save path.
     path += "variable_noise/"
 
+    # Initialize the title of the plot.
     title = "Variable Noise"
 
+    # Create a figure with a grid of subplots.
     fig, ax = plt.subplots(plot_grid_size, plot_grid_size, figsize=(6, 6))
 
+    # Hide the x and y axes of all subplots.
     for i, j in itertools.product(range(plot_grid_size), range(plot_grid_size)):
         ax[i, j].get_xaxis().set_visible(False)
         ax[i, j].get_yaxis().set_visible(False)
 
+    # Plot each of the generated images in its own subplot.
     for sample in range(num_test_samples):
         i = sample // 4
         j = sample % 4
@@ -77,6 +105,9 @@ def plot_images(epoch, path, num_test_samples, generator, device):
             cmap="Greys",
         )
 
+    # Add a label for the epoch at the bottom of the plot.
     label = "Epoch_{}".format(epoch + 1)
     fig.text(0.5, 0.04, label, ha="center")
+
+    # Add the title to the plot.
     fig.suptitle(title)
