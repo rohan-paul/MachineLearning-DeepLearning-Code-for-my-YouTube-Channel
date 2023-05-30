d533a0f6b0201e35dd4542bbbaa71fc430ead22b
Rohan Paul <rohan-paul@users.noreply.github.com>
2023-05-30 21:45:18 +0530

    "updating"

---
+++ ML_Algos_From_Scratch/Lasso Regression from Scratch/test.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+
+def _linear_mode(X, theta, bias ):
+    return np.dot(X, theta) + bias
+
+def _initialize_parameters(n_features):
+    theta = np.random.random_sample(n_features)
+    bias = np.random.random_sample()
+
+def _initialize_parameters(n_features):
+    theta = np.random.random_sample()
+
+
