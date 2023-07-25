a569878b5075d5c35fa18faddae54751dbe2eeb4
Rohan Paul <rohan-paul@users.noreply.github.com>
2023-07-26 02:34:17 +0530

    updating

--- ML_Algos_From_Scratch/Linear Regression From Scratch with Pure Python/test.py
+++ ML_Algos_From_Scratch/Linear Regression From Scratch with Pure Python/test.py
@@ -1,18 +1,22 @@
-import ast
-import astor
-import re
+def compute_metrics(eval_preds):
+    preds, labels = eval_preds
+    if isinstance(preds, tuple):
+        preds = preds[0]
+    for idx in range(len(preds)):
+        for idx2 in range(len(preds[idx])):
+            if preds[idx][idx2]==-100:
+                preds[idx][idx2] = 50256
+    decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+    # Replace -100 in the labels as we can't decode them.
+    labels = np.where(labels != pad_tok, labels, tokenizer.pad_token_id)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
 
-def remove_docs_and_comments(file):
-    with open(file,"r") as f:
-        code = f.read()
-    parsed = ast.parse(code)
-    for node in ast.walk(parsed):
-        if isinstance(node, ast.Expr) and isinstance(node.value, ast.Str):
-            # set value to empty string
-            node.value = ast.Constant(value='')
-    formatted_code = astor.to_source(parsed)
-    pattern = r'^.*"""""".*$' # remove empty """"""
-    formatted_code = re.sub(pattern, '', formatted_code, flags=re.MULTILINE)
-    return formatted_code
+    # Some simple post-processing
+    decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
 
-remove_docs_and_comments("Linear_Regression_From_Scratch.py")
+    result = metric.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True)
+    #
+    result = {k: round(v * 100, 4) for k, v in result.items()}
+    prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds]
+    result["gen_len"] = np.mean(prediction_lens)
+    return result
